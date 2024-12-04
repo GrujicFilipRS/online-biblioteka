77f55e515d6229f13074ccefcc9b590ab602f5bd
GrygoryZach <grishazschepilo@gmail.com>
2024-12-04 13:59:03 +0100

    columns year, grade and path are added to Book table in the database

--- data/books.py
+++ data/books.py
@@ -9,7 +9,10 @@ class Book(SqlAlchemyBase, SerializerMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     uploaded_user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('user.id'), nullable=False)
+    path = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now)
 
     author_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('author.id'), nullable=False)
     description = sqlalchemy.Column(sqlalchemy.String)
+    year = sqlalchemy.Column(sqlalchemy.Integer)
+    grade = sqlalchemy.Column(sqlalchemy.Integer)
