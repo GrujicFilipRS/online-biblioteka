f61fefc046e664d9458ed440a2016bf1eb6bcbd9
GrygoryZach <grishazschepilo@gmail.com>
2024-12-04 22:19:03 +0100

    images column added to Authors table

--- data/users.py
+++ data/users.py
@@ -14,6 +14,7 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     nickname = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, default=None)
+    image = sqlalchemy.Column(sqlalchemy.String, default="https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg")
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
