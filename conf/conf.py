cd32f064ec42244ad543f53bea97b84f30f71266
yxzhin <yxzhin@gmail.com>
2024-12-05 15:48:53 +0100

    add image search to quotes

--- conf/conf.py
+++ conf/conf.py
@@ -3,5 +3,6 @@
 from random import randint
 
 API_KEY_QUOTES = "VyD6UxNvDSGVV9hAYgZWTg==wZwWq7VjZWjedOnl"
-API_KEY_IMAGES = "blZ3LLVWpdAnkcW4rXdUtOirdoX9vUG3busRDAQIePVDpO95xTszZSzn"
+API_KEY_IMAGES = "AIzaSyDGxYznV6zuUUKP6SrnJNma2EKmdt_qrGA"
+API_CX_IMAGES = "143f69530b2634ac4"
 SECRET_KEY = f"GOYDA_{randint(1000, 9999)}"
